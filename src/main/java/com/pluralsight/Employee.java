cf465ac85c9cf5242176886ae1746003ab8be539
pmvroque15 <pmvroque@gmail.com>
2026-05-05 14:41:47 -0400

    Edited employee java with punch out and punch in derive attribuutes

--- src/main/java/com/pluralsight/Employee.java
+++ src/main/java/com/pluralsight/Employee.java
@@ -6,6 +6,8 @@ public class Employee {
     private String department;
     private double payRate;
     private double hoursWorked;
+    public boolean isClockedIn;
+    public int clockInTime;
 
     public Employee(int employeeId, String name, String department, double payRate, double hoursWorked) {
         this.employeeId = employeeId;
@@ -35,6 +37,22 @@ public class Employee {
         return this.hoursWorked;
     }
 
+    public int getClockInTime() {
+        return clockInTime;
+    }
+
+    public void setClockInTime(int clockInTime) {
+        this.clockInTime = clockInTime;
+    }
+
+    public boolean isClockedIn() {
+        return isClockedIn;
+    }
+
+    public void setClockedIn(boolean clockedIn) {
+        isClockedIn = clockedIn;
+    }
+
     public double getTotalPay() {
         return this.payRate * this.hoursWorked;
     }
@@ -55,13 +73,28 @@ public class Employee {
         return 0;
     }
 
-//    public double punchIn(int time) {
+    //int time = 10 for 10:00 am
+    //int time = 12 for 12:00 pm
+    //int time = 14 for 2:00 pm
+//    public void punchIn(int time) {
 //
 //    }
-//    public double punchOut(int time) {
+//
+//    public void punchOut(int time) {
 ////        When they punch out, we calculate how many hours they have worked and add that
 ////        time to their hours worked.
-////        int timeWorked = time - punchInTime;
-////        return this.hoursWorked += time;
+//
 //    }
+    public void punchTimeCard(int time) {
+        if (!isClockedIn) {
+            clockInTime = time;
+            isClockedIn = true;
+        } else if(isClockedIn) {
+            int hours = time - clockInTime;
+            hoursWorked += hours;
+
+            isClockedIn = false;
+        }
+
+    }
 }
--- src/main/java/com/pluralsight/Room.java
+++ src/main/java/com/pluralsight/Room.java
@@ -28,37 +28,28 @@ public class Room {
         return !isOccupied && !isDirty;
     }
 
-    public boolean checkIn(Guest guest) {
+    public void checkIn() {
         //if the room is checked in
         //mark occupied as true and mark as dirty
         if(!isOccupied && !isDirty) {
             isOccupied = true;
             isDirty = true;
-            return true;
         }
-
-        return false;
     }
-    public boolean checkOut() {
+    public void checkOut() {
         if (isOccupied) {
             isOccupied = false;
             isDirty = true;
-            return true;
         }
-
-        return false;
     }
-    public boolean cleanRoom() {
+    public void cleanRoom() {
         if (!isOccupied && isDirty) {
             isDirty = false;
-            return true;
         }
-
-        return false;
     }
 
     public boolean isDirty() {
-        return isOccupied;
+        return isDirty;
     }
 
 
